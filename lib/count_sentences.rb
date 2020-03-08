c94167f3326a88e54f05d4379ea9103996db1ada
Chukwuemeka Kanu <chukwuemekakanu@gmail.com>
2020-03-08 21:57:02 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,15 +3,21 @@
 class String
 
   def sentence?
-   self.end_with?(".")
+    self.end_with?(".")
   end
 
   def question?
-   self.end_with?("?")
+    self.end_with?("?")
   end
-
+  
   def exclamation?
-   self.end_with?("!")
+    self.end_with?("!")
   end
+  
+   def count_sentences
+    self.split(/\. |\?|!/).length
+  end 
+  
+
 
 end
